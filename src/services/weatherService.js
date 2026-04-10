72295c1e24a9522c3bd3c1f78e23e75e30e678c6
Nastia Klochko <emanon2015@gmail.com>
2026-04-10 18:28:33 +0300

    Setup + architecture, API + base state (1-2)

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,16 +1,14 @@
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
-const BASE_URL = "https://api.weatherapi.com/v1";
-
-export const getWeather = async (city) => {
+export const getForecast = async (city) => {
   try {
     const response = await fetch(
-      `${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=7`
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&lang=ua&appid=${API_KEY}`
     );
 
     const data = await response.json();
     return data;
   } catch (error) {
-    console.error("Error fetching weather:", error);
+    console.error("Error fetching forecast:", error);
   }
 };
